238e1708b45b50b5b5bef3fa92e9df57ba65b656
korrakod1712 <korrakod.rrpn@gmail.com>
2024-12-23 08:50:45 +0700

    commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
-import router from './router';// Import Bootstrap and BootstrapVue3 CSS files
+import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 import piniaPersistedState from 'pinia-plugin-persistedstate';
